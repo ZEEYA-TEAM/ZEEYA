0c4220b128c5f7642db5f60fcc92b8015659fb57
Ziano <Ziano@ZIA>
2024-03-11 09:24:25 +0100

    Login

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+function Login(){
+    const [firstName, setFirstName] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    if (firstName.trim() !== "") {
+      setIsLoggedIn(true);
+    }
+};
+    return(
+        <div>
+            {isLoggedIn ? (
+        <p>Välkommen, {firstName}!</p>
+      ) : (
+        <form>
+          <label>
+            Förnamn:
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="button" onClick={handleLogin}>
+            Logga in
+          </button>
+        </form>
+      )}
+        </div>
+    )
+}
+export default Login;

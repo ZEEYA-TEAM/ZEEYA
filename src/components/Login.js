e382103bfcbc4d47e02e5490779eceed31cf1fe6
Ziano <Ziano@ZIA>
2024-03-12 17:59:41 +0100

    style

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,25 +11,37 @@ function Login() {
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      {isLoggedIn ? (
-        <p>Välkommen, {firstName}!</p>
-      ) : (
-        <form>
-          <label>
-            Förnamn:
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </label>
-          <br />
-          <button type="button" onClick={handleLogin}>
-            Logga in
-          </button>
-        </form>
-      )}
+    <div>
+      <nav >
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li> <a href="#">About</a></li>
+          <li> <a href="#">Contact</a></li>
+          <li> <a href="#">Projects</a></li>
+          <li> <a href="#">Todo</a></li>
+          <li> <a href="#">New Todo</a></li>
+        </ul>
+      </nav>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh"}}>
+        {isLoggedIn ? (
+          <p>Välkommen, {firstName}!</p>
+        ) : (
+          <form>
+            <label>
+              Förnamn:
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="button" onClick={handleLogin}>
+              Logga in
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }

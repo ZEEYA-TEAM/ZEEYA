afd3fa5ea8aaec2ca379c12054d2fee137e975a4
Ziano <Ziano@ZIA>
2024-03-14 10:00:07 +0100

    AddDate

---
+++ src/components/AddDateProjects.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+function AddDateProjects(){
+    const[projects, setProjects]= useState("")
+    const DateChange = (event)=>{
+        setProjects(event.target.value);
+    };
+    const handelSubmit = (event)=>{
+        event.preventDefault();
+    };
+    return(
+        <form onSubmit={handelSubmit}>
+            <input type="projects" value={projects}>DateChange</input>
+            <button type="submit">Download projects</button>
+
+        </form>
+    );
+}
+export default AddDateProjects;

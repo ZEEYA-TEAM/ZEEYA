281867d50af8280cc971fe5d3f950166bc056aea
Alexander Doja <alexander.doja@outlook.com>
2024-03-20 12:20:12 +0100

    Sass and style changes etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import People from "./components/People";
 
 function App() {
+  const mainRef = useRef();
+  useEffect(() => {
+    const setMinHeight = () => {
+      if (!mainRef.current) return;
+      mainRef.current.style.minHeight = `${window.innerHeight}px`;
+    };
+    setMinHeight();
+    window.addEventListener("resize", setMinHeight);
+    return () => {
+      window.removeEventListener("resize", setMinHeight);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
-      <Login />      
+      <Login />    
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,9 @@ function Login() {
   const [firstName, setFirstName] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    
     if (firstName.trim() !== "") {
       LoggingService.logUserAction(firstName, 'button_click', 'Login', 'Login-button');
       setIsLoggedIn(true);
@@ -14,31 +16,30 @@ function Login() {
   };
   return (
     <>
-      <div>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh"}}>
-          {isLoggedIn ? (
-           <div>
-           <p style={{fontSize:"2em"}}>Welcome, {firstName}!</p>
-           <Layout />
-           </div>
-          ) : (            
-            <form>
-              <label>
-                First Name:
-                <input
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </label>
-              <br />
-              <button type="button" onClick={handleLogin}>
-                Logga in
-              </button>
-            </form>
-          )}
-        </div>
-      </div>
+      <main>
+        {isLoggedIn ? (
+          <div>
+          <p style={{fontSize:"2em"}}>Welcome, {firstName}!</p>
+          <Layout />
+          </div>
+        ) : (
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              aria-label="Name"
+              autoComplete="username"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />      
+            <button type="submit">
+              Logga in
+            </button>
+          </form>
+        )}
+      </main>
     </>
   );
 }

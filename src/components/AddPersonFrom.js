daff4c7e01537f2616eb32dcec07cb7d7dfbf04c
Alexander Doja <alexander.doja@outlook.com>
2024-03-20 10:25:03 +0100

    Reconsile changes

--- src/components/AddPersonFrom.js
+++ src/components/AddPersonFrom.js
@@ -5,16 +5,20 @@ function AddPersonForm() {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
 
+  // Definiera en funktion som heter handleSubmit som tar en händelse (event) som indata
   const handleSubmit = async (event) => {
+     // Förhindra standardbeteendet för händelsen, vilket i detta fall förhindrar att sidan omladdas när formuläret skickas in
     event.preventDefault();
+     // Skapa en ny variabel newPerson som innehåller ett objekt med egenskaperna name och role
     const newPerson = {
-      name: name,
-      role: role
+      name: name, //Hämta värdet från variabeln name och tilldela det till egenskapen name i newPerson
+      role: role //Hämta värdet från variabeln role och tilldela det till egenskapen role i newPerson
     };
 
     try {
-      const response = await sendPerson(newPerson.name, newPerson.role);
-      console.log("Data sparad i Notion: ", response.data);
+      await sendPerson(newPerson.name, newPerson.role);
+      alert("Data sparad");
+      console.log("Data sparad i Notion:", newPerson);
       setName("");
       setRole("");
     } catch (error) {

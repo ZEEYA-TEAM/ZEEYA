d9357891af0e87cf4bb92e9d08503e9895cfa8ec
Ziano <Ziano@ZIA>
2024-03-20 10:11:50 +0100

    z

--- src/components/AddPersonFrom.js
+++ src/components/AddPersonFrom.js
@@ -5,16 +5,19 @@ function AddPersonForm() {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
 
+  // Definiera en funktion som heter handleSubmit som tar en händelse (event) som indata
   const handleSubmit = async (event) => {
+     // Förhindra standardbeteendet för händelsen, vilket i detta fall förhindrar att sidan omladdas när formuläret skickas in
     event.preventDefault();
+     // Skapa en ny variabel newPerson som innehåller ett objekt med egenskaperna name och role
     const newPerson = {
-      name: name,
-      role: role
+      name: name, //Hämta värdet från variabeln name och tilldela det till egenskapen name i newPerson
+      role: role //Hämta värdet från variabeln role och tilldela det till egenskapen role i newPerson
     };
 
     try {
       await sendDataToNotion(newPerson);
-      alert("Data sparad");
+      alert(`Welcome: ${newPerson.name} Role: ${newPerson.role}`);
       console.log("Data sparad i Notion:", newPerson);
     } catch (error) {
       console.error("Fel vid sparande av data i Notion:", error);
